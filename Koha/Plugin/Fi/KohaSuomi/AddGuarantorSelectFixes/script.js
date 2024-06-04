fe7f3e5985ae0d8879185e3a956985723b4532eb
lmstrand <lmstrand@gmail.com>
2024-06-04 14:02:22 +0300

    Add script

--- Koha/Plugin/Fi/KohaSuomi/AddGuarantorSelectFixes/script.js
+++ Koha/Plugin/Fi/KohaSuomi/AddGuarantorSelectFixes/script.js
@@ -1,1 +1,4 @@
-
+$(document).on('click', '#guarantor_search', function(e) {
+    e.preventDefault();
+    var newin = window.open('/cgi-bin/koha/members/search.pl?columns=action,cardnumber,name,category,branch,dateofbirth,address','popup','width=1024,height=768,resizable=no,toolbar=false,scrollbars=yes,top');
+});
